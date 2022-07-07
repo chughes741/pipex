71c9dab6c94bcc8d7d3e6bdb3e3c6593558aa588
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-07-07 15:22:34 -0400

    Added include folder

--- include/pipex.h
+++ include/pipex.h
@@ -1,18 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chughes <marvin@42quebec.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/06 13:04:17 by chughes           #+#    #+#             */
-/*   Updated: 2022/07/06 13:04:50 by chughes          ###   ########.fr       */
+/*   Created: 2022/07/06 12:58:16 by chughes           #+#    #+#             */
+/*   Updated: 2022/07/06 13:04:02 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/pipex.h"
+#ifndef PIPEX_H
+# define PIPEX_H
 
-int	main(int c, char *args[])
-{
-	return (0);
-}
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h> 
+# include <sys/wait.h>
+
+# include "ft_printf/ft_printf.h"
+
+#endif
