77fb57b785690a942cbddeaf3c0336772aefa03c
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-07-10 17:50:51 -0400

    Testing out execve and fork instructions

--- src/birth_child.c
+++ src/birth_child.c
@@ -1,31 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   birth_child.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chughes <marvin@42quebec.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/06 13:04:17 by chughes           #+#    #+#             */
-/*   Updated: 2022/07/06 13:04:50 by chughes          ###   ########.fr       */
+/*   Created: 2022/07/08 16:00:23 by chughes           #+#    #+#             */
+/*   Updated: 2022/07/08 16:00:24 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-int	main(int c, char *args[])
-{
-	t_data	*data;
-	char	*path = "/bin/bash";
-	char	*exec_args[] = {path, "-c", "echo \"Visit $HOSTNAME from your browser.\"", NULL};
-	char	*env[] = {NULL};
-	int		parent;
-
-	parent = fork();
-	if (parent)
-		fork();
-	data = get_data();
-	data->fd_input = c;
-	args = NULL;
-	execve(path, exec_args, env);
-	return (0);
-}
