ff9400b229511c6e1211cdeeeeac9861bd6e31d6
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-07-11 16:12:01 -0400

    Testing out fork and pipe

--- src/pipex.c
+++ src/pipex.c
@@ -12,20 +12,27 @@
 
 #include "../include/pipex.h"
 
-int	main(int c, char *args[])
+int	main(int argc, char *argv[])
 {
 	t_data	*data;
-	char	*path = "/bin/bash";
-	char	*exec_args[] = {path, "-c", "echo \"Visit $HOSTNAME from your browser.\"", NULL};
-	char	*env[] = {NULL};
-	int		parent;
 
-	parent = fork();
-	if (parent)
-		fork();
 	data = get_data();
-	data->fd_input = c;
-	args = NULL;
-	execve(path, exec_args, env);
+	data->fd_input = open(argv[1], 'r');
+	data->fd_output = open(argv[argc - 1], 'w');
+
+	char buffer[11];
+	read(data->fd_input, buffer, 10);
+	printf("%s\n", buffer);
+	char buffer2[11];
+	read(data->fd_output, buffer2, 10);
+	printf("%s\n", buffer2);
+
 	return (0);
+}
+
+void	child(char *argv[])
+{
+	char	**exec_args = ft_split(argv[3], ' ');
+
+	execve("/bin/bash", exec_args, NULL);
 }
