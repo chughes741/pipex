2c4f4369835e48866442e7f82227593f813998dd
Cole - MBA <coltenhughes@gmail.com>
2022-07-18 12:58:33 -0400

    Added voided input to make compile

--- src/check_input.c
+++ src/check_input.c
@@ -7,5 +7,7 @@ void	check_input(int argc, char *argv[])
 {
 	if (argc < 5)
 		exit_error("Error: Too few arguments ");
+	if (!argv[0]) //! Void for compiling
+		exit_error("This shouldn't be possible");
 	return ;
 }
