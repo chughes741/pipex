4cc5f2a2c6269c97c33387c1a5130ab6ce8564c0
Cole - MBA <coltenhughes@gmail.com>
2022-07-25 19:11:55 -0400

    Moved init_data to its own file

--- src/get_data.c
+++ src/get_data.c
@@ -21,39 +21,3 @@ t_data	*get_data(void)
 		data = ft_calloc(1, sizeof(t_data));
 	return (data);
 }
-
-// Returns a new string with the PATH from envp
-static char	**find_path(char *envp[])
-{
-	char	*path;
-	char	**paths;
-	int		i;
-
-	i = 0;
-	while (ft_strncmp(envp[i], "PATH=", 5))
-		i++;
-	path = ft_strdup(&envp[i][5]);
-	paths = ft_split(path, ':');
-	free(path);
-	return (paths);
-}
-
-// Initializes data struct
-void	init_data(int argc, char *argv[], char *envp[])
-{
-	t_data	*data;
-
-	data = get_data();
-	if (argc != 5)
-		exit_error("Error: Too few arguments ");
-	data->argv = argv;
-	data->envp = envp;
-	data->fd_in = open(argv[1], O_RDONLY);
-	if (data->fd_in < 0 || access(argv[1], R_OK) < 0)
-		exit_error("Error with input file ");
-	data->fd_out = open(argv[4], O_RDWR | O_CREAT | O_TRUNC, 0644);
-	if (data->fd_out < 0)
-		exit_error("Error with output file ");
-	data->paths = find_path(envp);
-	return ;
-}
