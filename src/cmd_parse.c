9b07b68695a72f8b60d09acb2b9627d68a7065ab
Cole Hughes <95713039+chughes741@users.noreply.github.com>
2022-08-03 21:18:10 -0400

    Added list functions to cmd parse

--- src/cmd_parse.c
+++ src/cmd_parse.c
@@ -21,11 +21,9 @@
 // Duplicates contents from cmds into rtn, frees cmds
 static void lst_to_str(char ***rtn, t_list *cmds)
 {
-	t_list	*head;
 	t_list	*node;
 	int		i;
 
-	head = cmds;
 	node = cmds;
 	i = -1;
 	while (node && *rtn[++i])
@@ -33,8 +31,43 @@ static void lst_to_str(char ***rtn, t_list *cmds)
 		*rtn[i] = ft_strdup(node->content);
 		node = node->next;
 	}
-	ft_lstclear(cmds, free());
+	while (cmds)
+	{
+		while ((node->next)->next)
+			node = node->next;
+		free((node->next)->content);
+		free(node->next);
+		node->next = NULL;
+		node = cmds;
+	}
 	return ;
+}
+
+// Returns index of first instance of char in c
+static int len_to_c(char *str, char *set)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (ft_strchr(set, str[i]))
+			break ;
+		i++;
+	}
+	return (i);
+}
+
+// Returns index of end of first cmd
+static int first_len(char *str)
+{
+    int     i;
+
+    i = 0;
+	if (len_to_c(str, " "))
+    while (str[i] && str[i] != ' ');
+		i++;
+    return (i);
 }
 
 // Beep boop write better docstrings Cole
@@ -43,14 +76,10 @@ char **cmd_parse(char *cmd)
 	t_list	*cmds;
 	char	**rtn;
 
-	if (is_quote(cmd, 0))
-		cmds = ft_lstnew(until next matching quote)
-	else
-		cmds = ft_lstnew(until first space)
-	if (start of cmd is './')
-		remove './' from cmds->contents
+	cmds = ft_lstnew(ft_substr(cmd, 0, first_len(cmd)));
 
-	rtn = ft_calloc(ft_lstsize(cmds) + 1, sizeof(char *))
+
+	rtn = ft_calloc(ft_lstsize(cmds) + 1, sizeof(char *));
 	lst_to_str(&rtn, cmds);
 	return (rtn);
 }
